cd7e73bdf2bf59752601c9c22ba9b1f8ad955e59
CarolSaldanha <carolsfernandes@outlook.com>
2024-12-06 22:57:38 -0300

    comitando

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from "./components/header";
+import ListarTarefa from "./pages/tarefa/ListarTarefa";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <ListarTarefa />
+    </div>
+  );
+}
+
+export default App;
